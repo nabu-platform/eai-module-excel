52e397c3e1113979470e7a59e354a2328fa47d60
Alexander Verbruggen <alex@nabu.be>
2016-11-24 17:54:14 +0100

    Updated to use collection based instead of array based utilities

--- src/main/java/nabu/data/excel/Services.java
+++ src/main/java/nabu/data/excel/Services.java
@@ -2,6 +2,7 @@ package nabu.data.excel;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class Services {
 	
 	@SuppressWarnings("resource")
 	@WebResult(name = "results")
-	public List<Object> toObject(@NotNull @WebParam(name = "typeId") String typeId, @WebParam(name = "workbook") Workbook workbook, @NotNull @WebParam(name = "sheet") String sheetName, @WebParam(name = "useRegexForSheet") Boolean useRegex, @WebParam(name = "fromRow") Integer fromRow, @WebParam(name = "toRow") Integer toRow, @WebParam(name = "columnsToIgnore") List<Integer> columnsToIgnore, @WebParam(name = "rotate") Boolean rotate, @WebParam(name = "includeEmptyResults") Boolean includeEmptyResults) throws IOException {
+	public List<Object> toObject(@NotNull @WebParam(name = "typeId") String typeId, @WebParam(name = "workbook") Workbook workbook, @NotNull @WebParam(name = "sheet") String sheetName, @WebParam(name = "useRegexForSheet") Boolean useRegex, @WebParam(name = "fromRow") Integer fromRow, @WebParam(name = "toRow") Integer toRow, @WebParam(name = "columnsToIgnore") List<Integer> columnsToIgnore, @WebParam(name = "rotate") Boolean rotate, @WebParam(name = "includeEmptyResults") Boolean includeEmptyResults) throws IOException, ParseException {
 		DefinedType resolved = executionContext.getServiceContext().getResolver(DefinedType.class).resolve(typeId);
 		if (resolved == null) {
 			throw new IllegalArgumentException("Could not find the type: " + typeId);
@@ -55,9 +56,9 @@ public class Services {
 		if (sheet == null) {
 			throw new IllegalArgumentException("Can not find sheet: " + sheetName);
 		}
-		Object[][] parsed = excelParser.parse(sheet);
+		List<List<Object>> matrix = excelParser.matrix(sheet);
 		if (rotate != null && rotate) {
-			parsed = MatrixUtils.rotate(parsed);
+			matrix = MatrixUtils.rotate(matrix);
 		}
 		if (fromRow != null) {
 			excelParser.setOffsetX(fromRow);
@@ -67,19 +68,25 @@ public class Services {
 		}
 		List<Object> result = new ArrayList<Object>();
 		List<Element<?>> children = new ArrayList<Element<?>>(TypeUtils.getAllChildren((ComplexType) resolved));
-		for (int row = 0; row < parsed.length; row++) {
+		for (int row = 0; row < matrix.size(); row++) {
+			if (fromRow != null && row < fromRow) {
+				continue;
+			}
+			else if (toRow != null && row >= toRow) {
+				break;
+			}
 			ComplexContent newInstance = ((ComplexType) resolved).newInstance();
 			boolean isEmptyRow = true;
 			int elementCounter = 0;
-			for (int column = 0; column < parsed[row].length; column++) {
+			for (int column = 0; column < matrix.get(row).size(); column++) {
 				if (columnsToIgnore == null || !columnsToIgnore.contains(column)) {
 					// we ignore elements beyond the ones we can map, this could be empty cells or uninteresting data
 					if (elementCounter >= children.size()) {
 						break;
 					}
 					Element<?> element = children.get(elementCounter++);
-					newInstance.set(element.getName(), parsed[row][column]);
-					if (isEmptyRow && parsed[row][column] != null) {
+					newInstance.set(element.getName(), matrix.get(row).get(column));
+					if (isEmptyRow && matrix.get(row).get(column) != null) {
 						isEmptyRow = false;
 					}
 				}
