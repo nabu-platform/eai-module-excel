c30af23358c4db63341f45b48347d30bf28ec7bc
Alexander Verbruggen <alex@nabu.be>
2016-12-04 21:06:42 +0100

    Added support for header validation Added support for full-excel parse that is in sync with other unmarshal() services

--- src/main/java/nabu/data/excel/Services.java
+++ src/main/java/nabu/data/excel/Services.java
@@ -14,12 +14,15 @@ import javax.validation.constraints.NotNull;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import be.nabu.eai.repository.EAIResourceRepository;
+import be.nabu.libs.property.api.Value;
 import be.nabu.libs.services.api.ExecutionContext;
 import be.nabu.libs.types.TypeUtils;
 import be.nabu.libs.types.api.ComplexContent;
 import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.DefinedType;
 import be.nabu.libs.types.api.Element;
+import be.nabu.libs.types.properties.AliasProperty;
 import be.nabu.utils.excel.ExcelParser;
 import be.nabu.utils.excel.FileType;
 import be.nabu.utils.excel.MatrixUtils;
@@ -41,9 +44,25 @@ public class Services {
 		return new ExcelParser(workbook).getSheetNames(includeHidden != null && includeHidden);
 	}
 	
+	@WebResult(name = "unmarshalled")
+	public Object unmarshal(@WebParam(name = "input") @NotNull InputStream input, @WebParam(name = "excelType") FileType type, @NotNull @WebParam(name = "type") String typeId, @WebParam(name = "rotate") Boolean rotate, @WebParam(name = "includeEmptyResults") Boolean includeEmptyResults, @WebParam(name = "useHeaders") Boolean useHeaders, @WebParam(name = "validateHeaders") Boolean validateHeaders) throws IOException, ParseException {
+		Workbook workbook = parse(input, type, null);
+		ComplexType resolve = (ComplexType) EAIResourceRepository.getInstance().resolve(typeId);
+		ComplexContent newInstance = resolve.newInstance();
+		for (Element<?> child : TypeUtils.getAllChildren(resolve)) {
+			Value<String> property = child.getProperty(AliasProperty.getInstance());
+			String sheetName = property == null ? child.getName() : property.getValue();
+			if (sheetName.equals("*")) {
+				sheetName = ".*";
+			}
+			newInstance.set(child.getName(), toObject(typeId, workbook, sheetName, true, null, null, null, rotate, includeEmptyResults, useHeaders, validateHeaders));
+		}
+		return newInstance;
+	}
+	
 	@SuppressWarnings("resource")
 	@WebResult(name = "results")
-	public List<Object> toObject(@NotNull @WebParam(name = "typeId") String typeId, @WebParam(name = "workbook") Workbook workbook, @NotNull @WebParam(name = "sheet") String sheetName, @WebParam(name = "useRegexForSheet") Boolean useRegex, @WebParam(name = "fromRow") Integer fromRow, @WebParam(name = "toRow") Integer toRow, @WebParam(name = "columnsToIgnore") List<Integer> columnsToIgnore, @WebParam(name = "rotate") Boolean rotate, @WebParam(name = "includeEmptyResults") Boolean includeEmptyResults) throws IOException, ParseException {
+	public List<Object> toObject(@NotNull @WebParam(name = "typeId") String typeId, @WebParam(name = "workbook") Workbook workbook, @NotNull @WebParam(name = "sheet") String sheetName, @WebParam(name = "useRegexForSheet") Boolean useRegex, @WebParam(name = "fromRow") Integer fromRow, @WebParam(name = "toRow") Integer toRow, @WebParam(name = "columnsToIgnore") List<Integer> columnsToIgnore, @WebParam(name = "rotate") Boolean rotate, @WebParam(name = "includeEmptyResults") Boolean includeEmptyResults, @WebParam(name = "useHeaders") Boolean useHeaders, @WebParam(name = "validateHeaders") Boolean validateHeaders) throws IOException, ParseException {
 		DefinedType resolved = executionContext.getServiceContext().getResolver(DefinedType.class).resolve(typeId);
 		if (resolved == null) {
 			throw new IllegalArgumentException("Could not find the type: " + typeId);
@@ -74,6 +93,34 @@ public class Services {
 			}
 			else if (toRow != null && row >= toRow) {
 				break;
+			}
+			if (useHeaders != null && useHeaders && ((fromRow == null && row == 0) || (fromRow != null && row == fromRow))) {
+				if (validateHeaders != null && validateHeaders) {
+					int elementCounter = 0;
+					for (int column = 0; column < matrix.get(row).size(); column++) {
+						if (columnsToIgnore == null || !columnsToIgnore.contains(column)) {
+							// we ignore elements beyond the ones we can map, this could be empty cells or uninteresting data
+							if (elementCounter >= children.size()) {
+								break;
+							}
+							Element<?> element = children.get(elementCounter++);
+							Value<String> alias = element.getProperty(AliasProperty.getInstance());
+							// don't check if it is an entire wildcard
+							if (alias != null && alias.getValue().equals("*")) {
+								continue;
+							}
+							String expectedName = alias == null ? element.getName() : alias.getValue();
+							if (matrix.get(row).get(column) == null) {
+								throw new ParseException("The actual header is null, expecting '" + expectedName + "'", row);
+							}
+							String actualName = matrix.get(row).get(column).toString().trim();
+							if (!actualName.equals(expectedName)) {
+								throw new ParseException("The actual header '" + actualName + "' does not match the expected '" + expectedName + "'", column);
+							}
+						}
+					}
+				}
+				continue;
 			}
 			ComplexContent newInstance = ((ComplexType) resolved).newInstance();
 			boolean isEmptyRow = true;
